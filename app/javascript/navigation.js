10073b8046ebddeaa67ec6b206e9376795981abd
pym <pymsoft5@gmail.com>
2025-02-01 07:13:52 +0100

    Footer v1

--- app/javascript/navigation.js
+++ app/javascript/navigation.js
@@ -2,3 +2,26 @@ function toggleMenu() {
   const menu = document.getElementById("homeMenu");
   menu.classList.toggle("hidden");
 }
+
+// JavaScript to handle the Back to Top button
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the button
+  var backToTopButton = document.getElementById("backToTop");
+
+  // Show the button when the user scrolls down 100px from the top of the document
+  window.onscroll = function() {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+          backToTopButton.classList.remove("hidden");
+      } else {
+          backToTopButton.classList.add("hidden");
+      }
+  };
+
+  // When the user clicks on the button, scroll to the top of the document
+  backToTopButton.onclick = function() {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth" // Smooth scrolling
+      });
+  };
+});
